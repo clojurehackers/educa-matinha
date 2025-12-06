b9ae0f85f0cd0098c8a2848a3485d64d6c2cdeb8
Wallysson Oliveira <wallyssonryu@gmail.com>
2025-12-06 17:47:09 -0300

    FEAT: button

--- src/educa_matinha/main.cljs
+++ src/educa_matinha/main.cljs
@@ -8,3 +8,24 @@
   "Cruel")
 
 (js/console.log (what-kind?))
+
+(defonce game-state (atom {:started false}))
+
+(defn start-game
+  []
+  (swap! game-state assoc :started (not (:started @game-state))))
+
+(defn main-template []
+  (sab/html [:div.h1 (if (:started @game-state) "game started" "game not started")
+             (sab/html [:a.start-button {:onClick start-game}
+                        (if (:started @game-state)
+                          "QUIT"
+                          "START")])]))
+
+(let [node (.getElementById js/document "app")]
+  (defn renderer []
+    (.render js/ReactDOM (main-template) node)))
+
+(add-watch game-state :renderer (fn [_ _ _ _] (renderer)))
+
+(renderer)
