30b4c6fe848ef2e9a491ef8e639e32c593bef1d6
Wallysson Oliveira <wallyssonryu@gmail.com>
2026-03-07 18:02:50 -0300

    FEAT

--- src/educa_matinha/main.cljs
+++ src/educa_matinha/main.cljs
@@ -9,7 +9,11 @@
 
 (defn start-game
   []
-  (swap! game-state assoc :started? (not (:started? @game-state))))
+  (swap! game-state
+         (fn [state]
+           (-> state
+               (assoc :started? (not (:started? @game-state)))
+               (assoc :player {:pos {:row 30 :col 5}})))))
 
 (defn main-template []
   (sab/html
@@ -18,20 +22,38 @@
       [:div.grid-container
        (for [row (range 40)
              col (range 20)]
-         [:div.grid-cell
-          {:key (str row "-" col)
-           :style (merge
-                   ; For debug purpose only
-                   (if (:debug? @game-state)
-                     {:background-color "#f0f0f0"
-                      :border "1px solid #ddd"
-                      :box-sizing "border-box"}
-                     {})
-                   {:background-color (get-in @game-state [:grid row col])})}])]]
+         (let [player-pos (-> @game-state :player :pos)
+               is-player? (and (= row (:row player-pos))
+                               (= col (:col player-pos)))]
+           [:div.grid-cell
+            {:key (str row "-" col)
+             :style (merge
+                     ; For debug purpose only
+                     (if (:debug? @game-state)
+                       {:background-color "#f0f0f0"
+                        :border "1px solid #ddd"
+                        :box-sizing "border-box"}
+                       {})
+                     {:background-color
+                      (if is-player?
+                        "red"
+                        (get-in @game-state [:grid row col]))})}]))]]
      [:div
       [:div.h1 "game not started"]
       [:a.start-button {:onClick start-game}
        "START"]])))
+
+(defn jump [e]
+  (when (:started? @game-state)
+    (let [key (.-key e)
+          {:keys [row col]} (-> @game-state :player :pos)
+          new-pos {:row (if (<= row 1)
+                          0
+                          (- row 2))
+                   :col col}]
+      (when (= key " ")
+        (.preventDefault e)
+        (swap! game-state assoc-in [:player :pos] new-pos)))))
 
 (let [node (.getElementById js/document "app")]
   (defn renderer []
@@ -40,3 +62,6 @@
 (add-watch game-state :renderer (fn [_ _ _ _] (renderer)))
 
 (renderer)
+
+(.addEventListener js/document "keydown" jump)
+;; TODO: event listener keypress C-c d toggle debug
