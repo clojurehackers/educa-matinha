3a817fe36335183134fd64a27ef9c6027ea41038
Wallysson Oliveira <wallyssonryu@gmail.com>
2026-03-07 15:52:28 -0300

    FEAT: grid

--- src/educa_matinha/main.cljs
+++ src/educa_matinha/main.cljs
@@ -4,23 +4,34 @@
    [cljsjs.react.dom]
    [sablono.core :as sab :include-macros true]))
 
-(defn what-kind? []
-  "Cruel")
-
-(js/console.log (what-kind?))
-
-(defonce game-state (atom {:started? false}))
+(defonce game-state (atom {:started? false
+                           :debug? false}))
 
 (defn start-game
   []
   (swap! game-state assoc :started? (not (:started? @game-state))))
 
 (defn main-template []
-  (sab/html [:div.h1 (if (:started? @game-state) "game started" "game not started")
-             (sab/html [:a.start-button {:onClick start-game}
-                        (if (:started? @game-state)
-                          "QUIT"
-                          "START")])]))
+  (sab/html
+   (if (:started? @game-state)
+     [:div.center-container
+      [:div.grid-container
+       (for [row (range 40)
+             col (range 20)]
+         [:div.grid-cell
+          {:key (str row "-" col)
+           :style (merge
+                   {:background-image (get-in @game-state [:grid row col])}
+                   ; For debug purpose only
+                   (if (:debug? @game-state)
+                     {:background-color "#f0f0f0"
+                      :border "1px solid #ddd"
+                      :box-sizing "border-box"}
+                     {}))}])]]
+     [:div
+      [:div.h1 "game not started"]
+      [:a.start-button {:onClick start-game}
+       "START"]])))
 
 (let [node (.getElementById js/document "app")]
   (defn renderer []
