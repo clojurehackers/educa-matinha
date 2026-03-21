4a4b1aa77c14b7c72d7dcc171cb0be71af7372ca
Luana <luanarj30@gmail.com>
2026-03-21 15:25:50 -0300

    add trees

--- src/educa_matinha/main.cljs
+++ src/educa_matinha/main.cljs
@@ -15,29 +15,41 @@
                (assoc :started? (not (:started? @game-state)))
                (assoc :player {:pos {:row 30 :col 5}})))))
 
+(def left-tree-positions #{0 10 20 30 40})
+(def right-tree-positions #{0 10 20 30 40})
+
+(defn tree
+  [r c dir] 
+  (sab/html [:div.grid-cell
+             {:key   (str r "-" c)}
+             [:img {:src (str "../../images/" dir "-tree.png")}]]))
+
 (defn main-template []
   (sab/html
-   (if (:started? @game-state)
+   (if (:started? @game-state) 
      [:div.center-container
-      [:div.grid-container
-       (for [row (range 40)
-             col (range 20)]
-         (let [player-pos (-> @game-state :player :pos)
-               is-player? (and (= row (:row player-pos))
-                               (= col (:col player-pos)))]
-           [:div.grid-cell
-            {:key   (str row "-" col)
-             :style (merge
+        [:div.grid-container
+         (for [row (range 40)
+               col (range 20)]
+           (let [player-pos (-> @game-state :player :pos)
+                 is-player? (and (= row (:row player-pos))
+                                 (= col (:col player-pos)))]
+             [:div.grid-cell
+              {:key   (str row "-" col)
+               :style (merge
                                         ; For debug purpose only
-                     (if (:debug? @game-state)
-                       {:background-color "#f0f0f0"
-                        :border           "1px solid #ddd"
-                        :box-sizing       "border-box"}
-                       {})
-                     {:background-color
-                      (if is-player?
-                        "red"
-                        (get-in @game-state [:grid row col]))})}]))]]
+                       (if (:debug? @game-state)
+                         {:background-color "#f0f0f0"
+                          :border           "1px solid #ddd"
+                          :box-sizing       "border-box"}
+                         {})
+                       {:background-color
+                        (if is-player?
+                          "red"
+                          (get-in @game-state [:grid row col]))})}
+              
+              (when (and (= col 0) (contains? left-tree-positions row)) (tree row col "left"))
+              (when (and (= col 13) (contains? right-tree-positions row)) (tree row col "right"))]))]]
      [:div
       [:div.h1 "game not started"]
       [:a.start-button {:onClick start-game}
