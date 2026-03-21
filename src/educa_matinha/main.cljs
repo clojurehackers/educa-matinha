dc0dadbae324f446f8afd542fb2013bc55389bdb
Luana <luanarj30@gmail.com>
2026-03-21 16:58:40 -0300

    add gravity

--- src/educa_matinha/main.cljs
+++ src/educa_matinha/main.cljs
@@ -2,10 +2,16 @@
   (:require
    [cljsjs.react]
    [cljsjs.react.dom]
-   [sablono.core :as sab :include-macros true]))
+   [sablono.core :as sab :include-macros true]) 
+  #_(:import
+   [java.lang Thread]))
 
 (defonce game-state (atom {:started? false
                            :debug?   false}))
+
+(def left-tree-positions #{0 10 20 35})
+(def right-tree-positions #{0 10 20 30 49})
+(def floor 39)
 
 (defn start-game
   []
@@ -13,10 +19,7 @@
          (fn [state]
            (-> state
                (assoc :started? (not (:started? @game-state)))
-               (assoc :player {:pos {:row 30 :col 5}})))))
-
-(def left-tree-positions #{0 10 20 30 40})
-(def right-tree-positions #{0 10 20 30 40})
+               (assoc :player {:pos {:row floor :col 10}})))))
 
 (defn tree
   [r c dir] 
@@ -55,17 +58,30 @@
       [:a.start-button {:onClick start-game}
        "START"]])))
 
+(defn sleep [ms]
+  (js/Promise.
+   (fn [resolve _reject]
+     (js/setTimeout resolve ms))))
+
+(defn gravity []
+  (let [player-pos (-> @game-state :player :pos)
+        y (:row player-pos)]
+    (when (< y floor) 
+      (js/setTimeout (fn [] (do (swap! game-state assoc-in [:player :pos :row] (+ y 1))
+                                (println "going down: " (-> @game-state :player :pos))
+                                (gravity))) 25))))
+
 (defn jump [e]
   (when (:started? @game-state)
     (let [key               (.-key e)
           {:keys [row col]} (-> @game-state :player :pos)
-          new-pos           {:row (if (<= row 1)
-                                    0
-                                    (- row 2))
+          new-pos           {:row (if (= row floor) (- row 5) row)
                              :col col}]
       (when (= key " ")
         (.preventDefault e)
-        (swap! game-state assoc-in [:player :pos] new-pos)))))
+        (swap! game-state assoc-in [:player :pos] new-pos)
+        (println "player-pos" (-> @game-state :player :pos))
+        (gravity)))))
 
 (let [node (.getElementById js/document "app")]
   (defn renderer []
