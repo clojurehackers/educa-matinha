297f0ccc9071f52639a2b1ea06a59019e6f9ce72
Luana <luanarj30@gmail.com>
2026-04-25 16:19:26 -0300

    call gravity always

--- src/educa_matinha/main.cljs
+++ src/educa_matinha/main.cljs
@@ -10,8 +10,7 @@
                            :vert-vel 0
                            :hort-vel 0}))
 
-(defonce left-tree-positions #{{:y 1 :l 0 :r 6} {:y 10 :l 0 :r 6} {:y 20 :l 0 :r 6} {:y 35 :l 0 :r 6}})
-(defonce right-tree-positions #{{:y 1 :l 13 :r 19} {:y 10 :l 13 :r 19} {:y 20 :l 13 :r 19} {:y 30 :l 13 :r 19}})
+(defonce tree-positions #{{:y 1 :l 0 :r 6} {:y 10 :l 0 :r 6} {:y 20 :l 0 :r 6} {:y 35 :l 0 :r 6} {:y 1 :l 13 :r 19} {:y 10 :l 13 :r 19} {:y 20 :l 13 :r 19} {:y 30 :l 13 :r 19}})
 (def floor 39)
 (def g -1)
 (def ini-vel -5)
@@ -49,37 +48,11 @@
                             :position "absolute"
                             :background-color "blue"}}])))))
 
-(defn main-template []
-  (sab/html
-   (if (:started? @game-state)
-     [:div.center-container
-      [:div.grid-container
-       (map render-tree left-tree-positions)
-       (map render-tree right-tree-positions)
-       (render-player @game-state)
-       (when (:debug? @game-state)
-         (for [row (range 40)
-               col (range 20)]
-           [:div.grid-cell
-            {:key   (str row "-" col)
-             :style {:border           "1px solid #ddd" 
-                     :box-sizing       "border-box"}}]))]]
-     [:div
-      [:div.h1 "game not started"]
-      [:a.start-button {:onClick start-game}
-       "START"]])))
-
-(defn sleep [ms]
-  (js/Promise.
-   (fn [resolve _reject]
-     (js/setTimeout resolve ms))))
-
 (defn collides? 
   "given the y trajectory and the x position of the player, 
    returns true whether the player is on top of a tree"
   [y y-new x]
-  (let [next-tree     (->> left-tree-positions
-                           (into right-tree-positions)
+  (let [next-tree     (->> tree-positions
                            (filter #(<= (+ y 1) (:y %)))
                            (filter #(>= y-new (:y %)))
                            (filter #(<= x (:r %)))
@@ -116,10 +89,10 @@
         obstacle   (next-obstacle row y-new col)
         y-new      (min y-new obstacle)]
     (when (or started? (not= row obstacle))
-      (js/setTimeout (fn [] (do (swap! game-state assoc-in [:jumping?] false)
-                                (swap! game-state assoc-in [:vert-vel] new-vel)
-                                (swap! game-state assoc-in [:player :pos :row] y-new)
-                                (gravity))) 25))))
+      (swap! game-state merge {:jumping? false 
+                               :vert-vel new-vel 
+                               :player {:pos {:row y-new 
+                                              :col col}}}))) 25)
 
 (defn move [e]
   (when (:started? @game-state)
@@ -134,15 +107,35 @@
           (= key " ")
           (do
             (.preventDefault e)
-            (swap! game-state assoc :jumping? true)
-            (swap! game-state assoc :vert-vel ini-vel))
+            (swap! game-state merge {:jumping? true
+                                     :vert-vel ini-vel}))
           (= key "d")
           (do (.preventDefault e)
               (swap! game-state assoc-in [:player :pos] right-pos))
           (= key "a")
           (do (.preventDefault e)
-              (swap! game-state assoc-in [:player :pos] left-pos)))
-        (gravity)))))
+              (swap! game-state assoc-in [:player :pos] left-pos)))))))
+
+(defn main-template []
+  (sab/html
+   (if (:started? @game-state)
+     (do
+       (js/setTimeout (fn [] (gravity)) 25)
+       [:div.center-container
+        [:div.grid-container
+         (map render-tree tree-positions)
+         (render-player @game-state)
+         (when (:debug? @game-state)
+             (for [row (range 40)
+                   col (range 20)]
+               [:div.grid-cell
+                {:key   (str row "-" col)
+                 :style {:border           "1px solid #ddd"
+                         :box-sizing       "border-box"}}]))]])
+     [:div
+      [:div.h1 "game not started"]
+      [:a.start-button {:onClick start-game}
+       "START"]])))
 
 (let [node (.getElementById js/document "app")]
   (defn renderer []
