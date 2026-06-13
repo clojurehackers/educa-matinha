c3aa07f9acc1b3a6b7fffbf7a7e7ec4251d8d460
Luana <luanarj30@gmail.com>
2026-06-13 15:43:27 -0300

    remove jumping?

--- src/educa_matinha/main.cljs
+++ src/educa_matinha/main.cljs
@@ -5,17 +5,15 @@
    [sablono.core :as sab :include-macros true]))
 
 (defonce tree-positions #{{:y 1 :l 0 :r 6} {:y 10 :l 0 :r 6} {:y 20 :l 0 :r 6} {:y 35 :l 0 :r 6} {:y 1 :l 13 :r 19} {:y 10 :l 13 :r 19} {:y 20 :l 13 :r 19} {:y 30 :l 13 :r 19}})
-
+(def floor 39)
 (defonce game-state (atom {:started? false
                            :debug?   true
-                           :jumping? false
                            :vert-vel 0
-                           :hort-vel 0
+                           :player {:pos [10 floor] :vel [0 0]}
                            :trees tree-positions}))
 
 (defonce keys-down (atom #{}))
 
-(def floor 39)
 (def g -1)
 (def ini-vel -5)
 
@@ -26,8 +24,7 @@
   (swap! game-state
          (fn [state]
            (-> state
-               (assoc :started? (not (:started? @game-state)))
-               (assoc :player {:row floor :col 10})))))
+               (assoc :started? (not (:started? @game-state)))))))
 
 (defn render-tree
   [{:keys [y l]}]
@@ -42,15 +39,15 @@
 (defn render-player 
   [{:keys [player]}]
   (when player
-    (let [{:keys [row col]} player]
-        (sab/html [:div
-                   {:key   (str row "-" col)
-                    :style {:margin-top (to-px (* 16 row))
-                            :margin-left (to-px (* 16 col))
-                            :width "16px"
-                            :height "16px"
-                            :position "absolute"
-                            :background-color "red"}}]))))
+    (let [[col row] (:pos player)]
+      (sab/html [:div
+                 {:key   (str row "-" col)
+                  :style {:margin-top (to-px (* 16 row))
+                          :margin-left (to-px (* 16 col))
+                          :width "16px"
+                          :height "16px"
+                          :position "absolute"
+                          :background-color "red"}}]))))
 
 (defn collides? 
   "given the y trajectory and the x position of the player, 
@@ -86,18 +83,17 @@
     10000))
 
 (defn gravity []
-  (let [{vel               :vert-vel
-         started?          :jumping?
-         {:keys [row col]} :player}  @game-state
-        new-vel                      (- vel g)
-        y-new                        (+ row new-vel)
-        obstacle                     (next-obstacle row y-new col)
-        y-new                        (min y-new obstacle)]
-    (when (or started? (not= row obstacle))
-      {:jumping? false 
-       :vert-vel new-vel 
-       :player   {:row y-new 
-                  :col col}})))
+  (let [{vel           :vert-vel
+         player :player}  @game-state
+        [col row]                (:pos player)
+        _ (println player)
+        new-vel                  (- vel g)
+        y-new                    (+ row new-vel)
+        obstacle                 (next-obstacle row y-new col)
+        y-new                    (min y-new obstacle)]
+    (when (not= row obstacle)
+      {:vert-vel new-vel 
+       :player   {:pos [col y-new]}})))
 
 (defn remove-commands [e]
   (let [code (str (.-code e))
@@ -129,20 +125,17 @@
 
 (defn move []
   (when (:started? @game-state)
-    (let [{:keys [row col]} (-> @game-state :player)]
+    (let [[col row] (-> @game-state :player :pos)]
 
       (cond->> {}
         (contains? @keys-down "Space")
-        (merge {:jumping? true
-                :vert-vel ini-vel})
+        (merge {:vert-vel ini-vel})
 
         (contains? @keys-down "KeyD")
-        (merge {:player {:row row
-                         :col (if (< col 19) (+ col 1) col)}})
+        (merge {:player {:pos [(if (< col 19) (+ col 1) col) row]}})
 
         (contains? @keys-down "KeyA")
-        (merge {:player {:row row
-                         :col (if (> col 0) (- col 1) col)}})))))
+        (merge {:player {:pos [(if (> col 0) (- col 1) col) row]}})))))
 
 (defn update-trees [trees]
   {:trees (map (fn [tree] (if (> (:y tree) floor) (assoc tree :y -10) (assoc tree :y (+ 0.1 (:y tree))))) trees)})
